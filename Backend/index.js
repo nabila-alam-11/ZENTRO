10f50bc3936f17c4515312ea3e86b3f1d32aa128
Nabila Alam <nabilazaheer1198@gmail.com>
2025-04-19 08:07:05 +0530

    feat: Configure CORS for the app

--- Backend/index.js
+++ Backend/index.js
@@ -3,3 +3,13 @@ const app = express(); // It creates an app object to build a web server, used t
 
 const { initializeDatabase } = require("./db/db.connect");
 initializeDatabase();
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: "*",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json()); // MIDDLEWARE - It automatically parses incoming request bodies with JSON data and makes them available under req.body for further processing in route handlers.
