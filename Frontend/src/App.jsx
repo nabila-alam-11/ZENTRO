f00453290f3ff1b6d1c829b3883efff4661b4c55
Nabila Alam <nabilazaheer1198@gmail.com>
2025-04-23 09:01:03 +0530

    feat: implemented Quick actions container using pure CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import Sidebar from "../components/sidebar";
 import MenuIcon from "../src/assets/menu.png";
+import Lead from "../src/assets/lead.png";
+import Report from "../src/assets/checklist-task-budget.png";
+import Deals from "../src/assets/trust-alt.png";
 import useFetch from "./useFetch";
 
 const App = () => {
@@ -55,6 +58,32 @@ const App = () => {
           </div>
         </div>
         {/* Container 3 */}
+        <div className="container-3">
+          <h1>Quick Actions</h1>
+          <div className="display-flex actions">
+            <div className="action">
+              <div className="icon-wrapper">
+                <img src={Lead} />
+              </div>
+              <h3>New Lead</h3>
+              <p>Add new prospect</p>
+            </div>
+            <div className="action">
+              <div className="icon-wrapper">
+                <img src={Report} />
+              </div>
+              <h3>See Reports</h3>
+              <p>View earnings</p>
+            </div>
+            <div className="action">
+              <div className="icon-wrapper">
+                <img src={Deals} />
+              </div>
+              <h3>View Deals</h3>
+              <p>Search for new deals</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
